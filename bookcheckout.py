75fd4f3d27f01ff76ac212f31eb99c0b62cfa83a
samvelsiby <69617763+samvelsiby@users.noreply.github.com>
2021-06-29 08:08:29 +0530

    Add files via upload

---
+++ bookcheckout.py
@@ -0,0 +1,15 @@
+#----------------------------------------------bookchckout--------------------------------
+
+def bookcheckout(Data,bookname):
+     
+    x
+    k=0
+    for student_mark in Data:
+        if bookname==student_mark[0] and student_mark[5]== 0: 
+           k+=1
+           
+        if k==0:
+           
+
+
+     
